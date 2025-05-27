7b6600838b46008005fa06e61021bb1ad7be73bb
m125 <kaman.marcell.2009.10.28@gmail.com>
2025-05-27 17:10:57 +0200

    push

--- dice_range_expression/main.py
+++ dice_range_expression/main.py
@@ -11,11 +11,11 @@ dies=[2,3,4,6,8,10,20]
 def get_biggets_dice(num):
 	reverse=dies[:]
 	reverse.reverse()
-	if num%2==1:return 3,num-3 #ha páratlan akkor 3as kocka
 	for e in reverse:
 		
 		if num>=e:
-			return e,num-e#legnagyobbat visszaadja
+			return e
+		#legnagyobbat visszaadja
 		
 
 
@@ -23,12 +23,13 @@ def get_biggets_dice(num):
 for e in inputs:
 	min=int(e[0])
 	max=int(e[1])
-	diff=max-min+1
+	target=max-min+1
 	result=[]
-	while diff>0:
-		print(min,max,diff)
-		dice,diff=get_biggets_dice(diff)
+	while target-sum(result)>0:
+		target=max-min+1+len(result)
+		print(min,max,target-sum(result))
+		dice=get_biggets_dice(target-sum(result))
 		result.append(dice)
-	print(e,max-min+1,result,max-(max-min+sum(1 for e in result) ))
+	print(e,max-min+1,result,max-(max-min+len(result) ))
 	
 
